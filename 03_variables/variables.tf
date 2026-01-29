77742dd6d50642d38f4a72b178aed5cf0c4804ad
manideep <160306420+Mani0013@users.noreply.github.com>
2026-01-29 23:31:33 +0530

    exploring variables in terraform

---
+++ 03_variables/variables.tf
@@ -0,0 +1,11 @@
+variable "environment" {
+    description = "Environment Name"
+    type        = string
+    default     = "dev" 
+}
+
+variable "bucket_name" {
+    description = "Bucket Name"
+    type        = string
+    default     = "my-tf-bucket"
+}
